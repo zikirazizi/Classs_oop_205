c488b43cb2b5a78e59af47f2d6f9fb01f71901e3
zikirazizi <zikir.azizi.ft22@mail.umy.ic.id>
2023-04-10 16:31:20 +0700

    data matakuliah

--- Classs_oop_205/Classs_oop_205.cpp
+++ Classs_oop_205/Classs_oop_205.cpp
@@ -1,3 +1,18 @@
 #include <iostream>
 using namespace std;
 
+class Mahasiswa {
+public:
+    int nim;
+    string nama;
+    void tampil() {
+        cout << "NIM= " << nim;
+        cout << "\nNama= " << nama;
+    }
+};
+class Matakuliah {
+private:
+    int sks;
+    string kode;
+    string namaMK;
+public:
